6737166419e655b837ebe85adc3b107a9842a366
gmlastik@gmail.com <gmlastik@gmail.com@2925ec42-30bd-11de-96ab-f16d9912eac9>
2009-04-27 19:02:22 +0000

    - adding system info to svn

---
+++ trunk/systeminfo.h
@@ -0,0 +1,20 @@
+#ifndef SYSTEMINFO_H
+#define SYSTEMINFO_H
+
+#include <QObject>
+#include <QString>
+
+class SystemInfo : public QObject
+{
+public:
+	static SystemInfo* instance();
+	const QString& os() const { return os_str_; }
+        QString localHostName_();
+private:
+	SystemInfo();
+
+	static SystemInfo* instance_;
+	QString os_str_;
+};
+
+#endif
