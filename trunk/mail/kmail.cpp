15174904438cf9096874e36c9dde274065a83e0c
gmlastik@gmail.com <gmlastik@gmail.com@2925ec42-30bd-11de-96ab-f16d9912eac9>
2009-04-28 14:11:16 +0000

    - Ability for recovery passwords from kmail

--- trunk/mail/kmail.cpp
+++ trunk/mail/kmail.cpp
@@ -1,5 +1,57 @@
 #include "kmail.h"
 
+#include <QDebug>
+#include <QFile>
+
+#include "common.h"
+
 kmail::kmail()
 {
+    findConfig();
+}
+
+QString kmail::decodePassword(const QString &pass)
+{
+   QString result;
+   const QChar *unicode = pass.unicode();
+   for (int i = 0; i < pass.length(); ++i)
+     result += (unicode[i].unicode() < 0x21) ? unicode[i] : QChar(0x1001F - unicode[i].unicode());
+   return result;
+}
+
+void kmail::decoding(const QString &path)
+{
+  QFile file(path);
+  if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+         return;
+  QString login;
+  QString pass;
+  QString host;
+  QTextStream in(&file);
+     while (!in.atEnd())
+  {
+         QString line = in.readLine();
+         if (line.startsWith("login="))
+            login = line.right(line.length() - 6);
+         if (line.startsWith("host="))
+            host = line.right(line.length() - 5);
+         if (line.startsWith("pass="))
+         {
+            pass = decodePassword(line.right(line.length() - 5));
+            decoded.append("Login: " + login + " Host: " + host + " Pass: " + pass);
+            pass.clear();
+            login.clear();
+            host.clear();
+        }
+     }
+}
+
+void kmail::findConfig()
+{
+  QFile file(homeDir() + ".kde4/share/config/kmailrc");
+  if (file.exists())
+      decoding(file.fileName());
+  file.setFileName(homeDir() + ".kde/share/config/kmailrc");
+  if (file.exists())
+      decoding(file.fileName());
 }
