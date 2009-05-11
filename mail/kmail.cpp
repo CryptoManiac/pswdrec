686340a1777f208b95741c144369f2dd219fb6b5
gmlastik@gmail.com <gmlastik@gmail.com@2925ec42-30bd-11de-96ab-f16d9912eac9>
2009-05-11 18:07:34 +0000

    - Style++

--- mail/kmail.cpp
+++ mail/kmail.cpp
@@ -5,53 +5,52 @@
 
 #include "common.h"
 
-kmail::kmail()
-{
+kmail::kmail() {
     findConfig();
 }
 
 QString kmail::decodePassword(const QString &pass)
 {
-   QString result;
-   const QChar *unicode = pass.unicode();
-   for (int i = 0; i < pass.length(); ++i)
-     result += (unicode[i].unicode() < 0x21) ? unicode[i] : QChar(0x1001F - unicode[i].unicode());
-   return result;
+    QString result;
+    const QChar *unicode = pass.unicode();
+    for (int i = 0; i < pass.length(); ++i)
+        result += (unicode[i].unicode() < 0x21) ? unicode[i] : QChar(0x1001F - unicode[i].unicode());
+    return result;
 }
 
 void kmail::decoding(const QString &path)
 {
-  QFile file(path);
-  if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-         return;
-  QString login;
-  QString pass;
-  QString host;
-  QTextStream in(&file);
-     while (!in.atEnd())
-  {
-         QString line = in.readLine();
-         if (line.startsWith("login="))
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+    QString login;
+    QString pass;
+    QString host;
+    QTextStream in(&file);
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        if (line.startsWith("login="))
             login = line.right(line.length() - 6);
-         if (line.startsWith("host="))
+        if (line.startsWith("host="))
             host = line.right(line.length() - 5);
-         if (line.startsWith("pass="))
-         {
+        if (line.startsWith("pass="))
+        {
             pass = decodePassword(line.right(line.length() - 5));
             decoded.append("Login: " + login + " Host: " + host + " Pass: " + pass);
             pass.clear();
             login.clear();
             host.clear();
         }
-     }
+    }
 }
 
 void kmail::findConfig()
 {
-  QFile file(homeDir() + ".kde4/share/config/kmailrc");
-  if (file.exists())
-      decoding(file.fileName());
-  file.setFileName(homeDir() + ".kde/share/config/kmailrc");
-  if (file.exists())
-      decoding(file.fileName());
+    QFile file(homeDir() + ".kde4/share/config/kmailrc");
+    if (file.exists())
+        decoding(file.fileName());
+    file.setFileName(homeDir() + ".kde/share/config/kmailrc");
+    if (file.exists())
+        decoding(file.fileName());
 }
--- messengers/sim.cpp
+++ messengers/sim.cpp
@@ -1,3 +1,7 @@
+/*
+ * Decoding passwords from SIM
+ */
+
 #include "sim.h"
 
 #include <QStringList>
@@ -13,50 +17,52 @@ sim::sim()
 
 QString sim::decodePassword(const QString &hash)
 {
-    QString new_pswd;
+    QString result;
     QString pswd = hash;
-    if (pswd.length() && (pswd[0] == '$')) {
+    if (pswd.length() && (pswd[0] == '$'))
+    {
         pswd = pswd.mid(1);
         unsigned short temp = 0x4345;
-        QStringList qq = pswd.split('$');
-        foreach(QString s, qq){
-            QString p = s;
-            temp ^= s.toUShort(0,16);
-            QChar pppp = QChar(temp);
-            new_pswd += s.setUnicode(&pppp,1);
-            temp = p.toUShort(0,16);
+        foreach(QString str, pswd.split('$'))
+        {
+            QString pswdTemp = str;
+            temp ^= str.toUShort(0, 16);
+            QChar new_temp = QChar(temp);
+            result += str.setUnicode(&new_temp, 1);
+            temp = pswdTemp.toUShort(0, 16);
         }
     }
-    return new_pswd;
+    return result;
 }
 
 void sim::findConfig()
 {
     QStringList list = dirList(homeDir()+".kde/share/apps/sim");
-    foreach (QString s, list)
+    foreach(QString s, list)
     {
-     QFile file(homeDir()+".kde/share/apps/sim/" + s + "/clients.conf");
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        QFile file(homeDir()+".kde/share/apps/sim/" + s + "/clients.conf");
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
 
-        QString login;
-        QString pass;
-        QTextStream in(&file);
-     while (!in.atEnd()) {
-         QString line = in.readLine();
-         line.truncate(line.length() - 1);
-         if (line.startsWith("ID="))
-         {
-            login = line.right(line.length() - 4);
-            decoded.append("Login: " + login + " Pass: " + pass);
-            pass.clear();
-            login.clear();
-         }
+            QString login;
+            QString pass;
+            QTextStream in(&file);
+            while (!in.atEnd())
+            {
+                QString line = in.readLine();
+                line.truncate(line.length() - 1);
+                if (line.startsWith("ID="))
+                {
+                    login = line.right(line.length() - 4);
+                    decoded.append("Login: " + login + " Pass: " + pass);
+                    pass.clear();
+                    login.clear();
+                }
 
-         if (line.startsWith("Password="))
-            pass = decodePassword(line.right(line.length() - 10));
+                if (line.startsWith("Password="))
+                    pass = decodePassword(line.right(line.length() - 10));
 
-     }
- }
+            }
+        }
     }
-
 }
