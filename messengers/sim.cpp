59245f9f6786198bc87ff4b04bac266c10d8bf1e
gmlastik@gmail.com <gmlastik@gmail.com@2925ec42-30bd-11de-96ab-f16d9912eac9>
2009-05-21 18:37:27 +0000

    - XML - IS OUR FUTURE! - System and Sim modules are rewriting to xml returning

--- messengers/sim.cpp
+++ messengers/sim.cpp
@@ -5,26 +5,25 @@
 #include "sim.h"
 
 #include <QStringList>
-#include <QFile>
 #include <QTextStream>
+#include <QDebug>
 
 #include "common.h"
 
-sim::sim()
-{
+sim::sim():QObject() {
+    root = decoded.createElement("sim");
+    decoded.appendChild(root);
     findConfig();
+    qDebug()<< decoded.toString();
 }
 
-QString sim::decodePassword(const QString &hash)
-{
+QString decodePassword(const QString &hash) {
     QString result;
     QString pswd = hash;
-    if (pswd.length() && (pswd[0] == '$'))
-    {
+    if (pswd.length() && (pswd[0] == '$')) {
         pswd = pswd.mid(1);
         unsigned short temp = 0x4345;
-        foreach(QString str, pswd.split('$'))
-        {
+        foreach(QString str, pswd.split('$')) {
             QString pswdTemp = str;
             temp ^= str.toUShort(0, 16);
             QChar new_temp = QChar(temp);
@@ -35,34 +34,52 @@ QString sim::decodePassword(const QString &hash)
     return result;
 }
 
-void sim::findConfig()
-{
-    QStringList list = dirList(homeDir()+".kde/share/apps/sim");
-    foreach(QString s, list)
-    {
-        QFile file(homeDir()+".kde/share/apps/sim/" + s + "/clients.conf");
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
-        {
+void sim::createXML(QString &login, QString &pass) {
+QDomElement q = decoded.createElement("Account");
+root.appendChild(q);
+    QDomElement tag = decoded.createElement("Login");
+    q.appendChild(tag);
+    QDomText t = decoded.createTextNode(login);
+    tag.appendChild(t);
 
-            QString login;
-            QString pass;
-            QTextStream in(&file);
-            while (!in.atEnd())
-            {
-                QString line = in.readLine();
-                line.truncate(line.length() - 1);
-                if (line.startsWith("ID="))
-                {
-                    login = line.right(line.length() - 4);
-                    decoded.append("Login: " + login + " Pass: " + pass);
-                    pass.clear();
-                    login.clear();
-                }
+    tag = decoded.createElement("Password");
+    q.appendChild(tag);
+    t = decoded.createTextNode(pass);
+    tag.appendChild(t);
+}
 
-                if (line.startsWith("Password="))
-                    pass = decodePassword(line.right(line.length() - 10));
-
-            }
+void sim::decoding(QFile &file) {
+    QString login, pass;
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        line.truncate(line.length() - 1);
+        if (line.startsWith("ID=") || line.startsWith("Screen=")) {
+            login = line.right(line.length() - 4);
+            createXML(login, pass);
+            pass.clear();
+            login.clear();
         }
+
+        if (line.startsWith("Password="))
+            pass = decodePassword(line.right(line.length() - 10));
+
     }
 }
+
+void sim::findConfig() {
+    QStringList list = dirList(homeDir()+".kde/share/apps/sim");
+    foreach(QString s, list) {
+        QFile file(homeDir()+".kde/share/apps/sim/" + s + "/clients.conf");
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+            decoding(file);
+    }
+}
+
+sim* sim::instance() {
+    if (!instance_)
+        instance_ = new sim();
+    return instance_;
+}
+
+sim* sim::instance_ = NULL;
--- systeminfo.cpp
+++ systeminfo.cpp
@@ -4,129 +4,149 @@
 #include "systeminfo.h"
 
 #include <QDebug>
+#include <QtXml/QDomDocument>
 
-SystemInfo::SystemInfo() : QObject()
-{
-
-        os_str_ = "Unknown";
-	
-	// Detect
-
-	struct utsname u;
-	uname(&u);
-        os_str_ = u.sysname;
-
-	// get description about os
-	enum LinuxName {
-		LinuxNone = 0,
-
-		LinuxMandrake,
-		LinuxDebian,
-		LinuxRedHat,
-		LinuxGentoo,
-		LinuxSlackware,
-		LinuxSuSE,
-		LinuxConectiva,
-		LinuxCaldera,
-		LinuxLFS,
-
-		LinuxASP, // Russian Linux distros
-		LinuxALT,
-
-		LinuxPLD, // Polish Linux distros
-		LinuxAurox,
-		LinuxArch
-	};
-
-        enum OsFlags
-        {
-		OsUseName = 0,
-		OsUseFile,
-		OsAppendFile
-	};
-
-        struct OsInfo
-        {
-		LinuxName id;
-		OsFlags flags;
-		QString file;
-		QString name;
-	} osInfo[] = {
-		{ LinuxMandrake,	OsUseFile,	"/etc/mandrake-release",	"Mandrake Linux"	},
-		{ LinuxDebian,		OsAppendFile,	"/etc/debian_version",		"Debian GNU/Linux"	},
-		{ LinuxGentoo,		OsUseFile,	"/etc/gentoo-release",		"Gentoo Linux"		},
-		{ LinuxSlackware,	OsAppendFile,	"/etc/slackware-version",	"Slackware Linux"	},
-		{ LinuxPLD,		OsUseFile,	"/etc/pld-release",		"PLD Linux"		},
-		{ LinuxAurox,		OsUseName,	"/etc/aurox-release",		"Aurox Linux"		},
-		{ LinuxArch,		OsUseFile,	"/etc/arch-release",		"Arch Linux"		},
-		{ LinuxLFS,		OsAppendFile,	"/etc/lfs-release",		"LFS Linux"		},
-
-		// untested
-		{ LinuxSuSE,		OsUseFile,	"/etc/SuSE-release",		"SuSE Linux"		},
-		{ LinuxConectiva,	OsUseFile,	"/etc/conectiva-release",	"Conectiva Linux"	},
-		{ LinuxCaldera,		OsUseFile,	"/etc/.installed",		"Caldera Linux"		},
-
-		// many distros use the /etc/redhat-release for compatibility, so RedHat will be the last :)
-		{ LinuxRedHat,		OsUseFile,	"/etc/redhat-release",		"RedHat Linux"		},
-
-		{ LinuxNone,		OsUseName,	"",				""			}
-	};
-
-        for (int i = 0; osInfo[i].id != LinuxNone; i++)
-        {
-		QFileInfo fi( osInfo[i].file );
-                if ( fi.exists() )
-                {
-			char buffer[128];
-
-			QFile f( osInfo[i].file );
-			f.open( QIODevice::ReadOnly );
-			f.readLine( buffer, 128 );
-			QString desc(buffer);
-
-                        desc = desc.trimmed ();
-
-                        switch ( osInfo[i].flags )
-                        {
-				case OsUseFile:
-					os_str_ = desc;
-					break;
-				case OsUseName:
-					os_str_ = osInfo[i].name;
-					break;
-				case OsAppendFile:
-					os_str_ = osInfo[i].name + " (" + desc + ")";
-					break;
-			}
-
-			break;
-		}
-	}
+SystemInfo::SystemInfo() : QObject() {
 }
 
-QString SystemInfo::localHostName_()
-{
-        struct utsname u;
-        uname(&u);
-        QString hostName;
-        hostName.sprintf("%s", u.nodename);
-        return hostName;
+QString getOS() {
+
+    QString os_str_ = "Unknown";
+
+    // Detect
+
+    struct utsname u;
+    uname(&u);
+    os_str_ = u.sysname;
+
+    // get description about os
+    enum LinuxName {
+        LinuxNone = 0,
+
+        LinuxMandrake,
+        LinuxDebian,
+        LinuxRedHat,
+        LinuxGentoo,
+        LinuxSlackware,
+        LinuxSuSE,
+        LinuxConectiva,
+        LinuxCaldera,
+        LinuxLFS,
+
+        LinuxASP, // Russian Linux distros
+        LinuxALT,
+
+        LinuxPLD, // Polish Linux distros
+        LinuxAurox,
+        LinuxArch
+    };
+
+    enum OsFlags {
+        OsUseName = 0,
+        OsUseFile,
+        OsAppendFile
+    };
+
+    struct OsInfo {
+        LinuxName id;
+        OsFlags flags;
+        QString file;
+        QString name;
+    } osInfo[] = {
+        { LinuxMandrake,	OsUseFile,	"/etc/mandrake-release",	"Mandrake Linux"	},
+        { LinuxDebian,		OsAppendFile,	"/etc/debian_version",		"Debian GNU/Linux"	},
+        { LinuxGentoo,		OsUseFile,	"/etc/gentoo-release",		"Gentoo Linux"		},
+        { LinuxSlackware,	OsAppendFile,	"/etc/slackware-version",	"Slackware Linux"	},
+        { LinuxPLD,		OsUseFile,	"/etc/pld-release",		"PLD Linux"		},
+        { LinuxAurox,		OsUseName,	"/etc/aurox-release",		"Aurox Linux"		},
+        { LinuxArch,		OsUseFile,	"/etc/arch-release",		"Arch Linux"		},
+        { LinuxLFS,		OsAppendFile,	"/etc/lfs-release",		"LFS Linux"		},
+
+        // untested
+        { LinuxSuSE,		OsUseFile,	"/etc/SuSE-release",		"SuSE Linux"		},
+        { LinuxConectiva,	OsUseFile,	"/etc/conectiva-release",	"Conectiva Linux"	},
+        { LinuxCaldera,		OsUseFile,	"/etc/.installed",		"Caldera Linux"		},
+
+        // many distros use the /etc/redhat-release for compatibility, so RedHat will be the last :)
+        { LinuxRedHat,		OsUseFile,	"/etc/redhat-release",		"RedHat Linux"		},
+
+        { LinuxNone,		OsUseName,	"",				""			}
+    };
+
+    for (int i = 0; osInfo[i].id != LinuxNone; i++) {
+        QFileInfo fi( osInfo[i].file );
+        if ( fi.exists() ) {
+            char buffer[128];
+
+            QFile f( osInfo[i].file );
+            f.open( QIODevice::ReadOnly );
+            f.readLine( buffer, 128 );
+            QString desc(buffer);
+
+            desc = desc.trimmed();
+
+            switch ( osInfo[i].flags ) {
+                case OsUseFile:
+                    os_str_ = desc;
+                    break;
+                case OsUseName:
+                    os_str_ = osInfo[i].name;
+                    break;
+                case OsAppendFile:
+                    os_str_ = osInfo[i].name + " (" + desc + ")";
+                    break;
+            }
+
+            break;
+        }
+    }
+    return os_str_;
+}
+QString localHostName() {
+    struct utsname u;
+    uname(&u);
+    QString hostName;
+    hostName.sprintf("%s", u.nodename);
+    return hostName;
 }
 
-QString SystemInfo::kernelVersion()
-{
-        struct utsname u;
-        uname(&u);
-        QString kernel;
-        kernel = u.release;
-        return kernel;
+QString kernelVersion() {
+    struct utsname u;
+    uname(&u);
+    QString kernel;
+    kernel = u.release;
+    return kernel;
 }
 
-SystemInfo* SystemInfo::instance()
-{
-	if (!instance_) 
-		instance_ = new SystemInfo();
-	return instance_;
+QDomDocument SystemInfo::collect() {
+    QDomDocument doc;
+    QDomElement root = doc.createElement("System info");
+    doc.appendChild(root);
+
+
+    QDomElement tag = doc.createElement("Distro");
+    root.appendChild(tag);
+    QDomText t = doc.createTextNode(getOS());
+    tag.appendChild(t);
+
+
+    tag = doc.createElement("Kernel version");
+    root.appendChild(tag);
+    t = doc.createTextNode(kernelVersion());
+    tag.appendChild(t);
+
+    tag = doc.createElement("Host name");
+    root.appendChild(tag);
+    t = doc.createTextNode(localHostName());
+    tag.appendChild(t);
+
+    return doc;
+}
+
+SystemInfo* SystemInfo::instance() {
+    if (!instance_)
+        instance_ = new SystemInfo();
+    return instance_;
 }
 
 SystemInfo* SystemInfo::instance_ = NULL;
